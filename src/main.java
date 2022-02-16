c2b11c33a656e872e9437781ecfbc6f2b17f0c33
meric-cpm <meric.chenu@efrei.net>
2022-02-16 10:39:37 -0500

    Addition final version

--- src/main.java
+++ src/main.java
@@ -1,9 +1,5 @@
-import java.awt.*;
-import java.sql.*;
-import javax.swing.*;
-
 public class main {
-    public static void main(String[] args) throws SQLException, SQLSyntaxErrorException{
+    public static void main(String[] args){
         String  url = "jdbc:mysql://localhost:3306/?autoReconnect=true&useSSL=false";
         DatabaseConnection new_connection = new DatabaseConnection();
         new_connection.setConnection(url);
